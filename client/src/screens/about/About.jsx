af20f66ec89982db30c7c370cdb6501f73755cfb
elvedin123 <elvedinc97@gmail.com>
2022-03-07 17:51:18 -0500

    added about me component

--- client/src/screens/about/About.jsx
+++ client/src/screens/about/About.jsx
@@ -3,8 +3,13 @@ import aboutcss from "./About.module.css"
 export default function About() {
   return (
     <div id="about" className={aboutcss.maincontainer}>
+
       <div className={aboutcss.workline}>
         <span className={aboutcss.about}>about</span>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   )
