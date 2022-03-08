6fadbd5acfd3efebec04b78219af43abf2fb1cf3
elvedin123 <elvedinc97@gmail.com>
2022-03-07 21:27:59 -0500

    fixed resume

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import navcss from './Nav.module.css'
 import { RiLinkedinBoxLine } from 'react-icons/ri'
 import { RiGithubLine } from 'react-icons/ri'
 import { GoMail } from 'react-icons/go'
-
+import Pdf from "../../assets/resume.pdf"
 export default function Nav() {
   return (
     <div className={navcss.matrixcontainer} >
@@ -11,7 +11,7 @@ export default function Nav() {
         <a className={navcss.links} href="#home">Home</a>
         <a className={navcss.links} href="#projects">Projects</a>
         <a className={navcss.links} href="#about">About</a>
-        <a className={navcss.links} href="../../resume.pdf">resume</a>
+        <a className={navcss.links} href={Pdf} target='_blank' rel="noreferrer">resume</a>
         <a className={navcss.links} href="#contact">contact</a>
       </div>
       <div className={navcss.icons}>
