c83c4f1f28bbe3afe7fdaa5857e9da4abeed73d0
elvedin123 <elvedinc97@gmail.com>
2022-03-07 19:32:49 -0500

    finished form set up

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -2,12 +2,16 @@ import formcss from './Form.module.css'
 
 export default function Form() {
   return (
-    <form action="">
-      <input type="text" placeholder='Fullname' />
-      <br />
-      <input type="text" placeholder='Email' />
-      <br />
-      <textarea name="" placeholder='Message' id="" cols="30" rows="10"></textarea>
-    </form>
+    <div>
+      <form className={formcss.form} action="">
+        <input className={formcss.input} type="text" placeholder='Full Name' />
+        <br />
+        <input className={formcss.input} type="text" placeholder='Email' />
+        <br />
+        <textarea className={formcss.input} name="" placeholder='Message' id="" cols="30" rows="10"></textarea>
+        <button className={formcss.button}><h4 className={formcss.buttontext}>Send email</h4></button>
+      </form>
+
+    </div>
   )
 }
